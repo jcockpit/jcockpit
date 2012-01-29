0bc7d939be2ecf1145f4d5093111a8d90986941c
Jeremie Lagarde <jer@printstacktrace.org>
2012-01-29 21:40:46 +0100

    #3 : Creating war package.

---
+++ org.jcockpit.data/src/org/jcockpit/model/internal/Activator.java
@@ -0,0 +1,38 @@
+package org.jcockpit.model.internal;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.jcockpit.model"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
